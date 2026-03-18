77c84e9b38f92bece3d15d4850e3a0035ab8833c
peteldasuki-stack <peteldasuki@gmail.com>
2026-03-18 23:46:53 +0100

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -11,7 +11,7 @@ for item in raw_data:
     clean_item = item.lower().strip()
     application_inbox.append(clean_item)
 
-# 3. Process (FIFO)
+# 3. Process (FIFO)  
 while application_inbox:
     name = application_inbox.popleft()
     print(f"Approving: {name}")
